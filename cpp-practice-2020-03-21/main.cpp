ece7fc6a877f4b9e2259a8d16808fdaa99f70961
NH5 <holiavin.nikolai.nh5@gmail.com>
2020-03-21 10:09:40 +0300

    Add 21.03.2020 practice task

---
+++ cpp-practice-2020-03-21/main.cpp
@@ -0,0 +1,78 @@
+/* Kholiavin Nikolai, M3138 */
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+void prefix(const char *s, size_t len, size_t *p)
+{
+  p[0] = 0;
+  for (size_t k = 0, i = 1; i < len; i++)
+  {
+    while (k > 0 && s[k] != s[i])
+      k = p[k - 1];
+    if (s[k] == s[i])
+      k++;
+    p[i] = k;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    std::cerr << "usage: " << argv[0] << " <substring> <filename>\n";
+    return EXIT_FAILURE;
+  }
+
+  FILE *f = fopen(argv[2], "r");
+  if (!f)
+  {
+    perror("fopen failed");
+    return EXIT_FAILURE;
+  }
+
+  size_t
+    len = strlen(argv[1]),
+    *p = new size_t[len];
+  prefix(argv[1], len, p);
+  bool result = false;
+  size_t k = 0;
+
+  do
+  {
+    char buffer[8192];
+    size_t bytes_read = fread(buffer, sizeof(char), std::size(buffer), f);
+
+    if (bytes_read == 0)
+    {
+      if (ferror(f))
+      {
+        perror("fread failed");
+        fclose(f);
+        return EXIT_FAILURE;
+      }
+
+      break;
+    }
+
+    for (size_t i = 0; i < bytes_read; i++)
+    {
+      while (k > 0 && argv[1][k] != buffer[i])
+        k = p[k - 1];
+      if (buffer[i] == argv[1][k])
+        k++;
+      if (k == len)
+      {
+        result = true;
+        break;
+      }
+    }
+  } while (!result);
+
+  delete[] p;
+  fclose(f);
+
+  std::cout << std::boolalpha << result << std::endl;
+  return EXIT_SUCCESS;
+}
